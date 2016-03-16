81518c0cf4c473c191eacf80dc351ae8d1c05ec1
andres7764 <andres7764@hotmail.com>
2016-03-16 16:57:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,10 @@
 var express = require('express');
-<<<<<<< HEAD
    app = express();
    bodyParser  = require("body-parser");
    methodOverride = require("method-override");
    http = require("http");
-   //cors = require('cors');
    mongoose = require('mongoose');
-=======
-var bodyParser  = require("body-parser");
-var methodOverride = require("method-override");
-var argv = require('optimist').argv;
-    http = require("http");
-var	cors = require('cors');
-var app = express();
->>>>>>> origin/master
-
+   
   // mongoose.connect('mongodb://104.155.107.128:80/mydb', function(err,res){ 
   mongoose.connect('mongodb://localhost/boxoApp', function(err, res) {
     if(err) console.log("error"+ err);
@@ -22,7 +12,6 @@ var app = express();
 
     //app.use(cors());
     app.use(bodyParser.urlencoded({ extended: false }));  
-<<<<<<< HEAD
     app.use(bodyParser.json());
     app.use(methodOverride());
 
@@ -34,15 +23,6 @@ var app = express();
     app.get('/', function(req,res) {
       res.send("wellcome");
     })
-=======
-	app.use(bodyParser.json());
-	app.use(methodOverride());
-var connect = mongoose.connect('mongodb://' + argv.be_ip + ':80/boxoApp');
-// mongoose.connect('mongodb://104.155.107.128:80/mydb', function(err,res){ 
-    console.log(connect); 
-     
-var users = connect.model('BoxoUsers');
->>>>>>> origin/master
 
   var boxoRoutes = express.Router();
 
@@ -61,10 +41,5 @@ var users = connect.model('BoxoUsers');
 
 //Declaración de módelos
 //var users = mongoose.model('BoxoUsers');
-<<<<<<< HEAD
 app.listen(3000);
 console.log("Running server");  
-=======
-app.listen(3000, argv.fe_ip);
-console.log("Running server");
->>>>>>> origin/master
